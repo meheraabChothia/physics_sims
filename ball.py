1b68c6d26fa5d3b67f68a6ca092a3daf754793bf
meheraabChothia <meheraablol@gmail.com>
2026-06-25 13:35:20 +0530

    initial commit
    Signed-off-by: meheraabChothia <meheraablol@gmail.com>

---
+++ ball.py
@@ -0,0 +1,33 @@
+G = 9.81
+H = 1000  # meters
+
+
+# s=ut + 0.5a(t^2)
+# Should just need this tbh
+
+def calculate_position(time: float):
+    # time in seconds
+    dis = 0.5*G*(time**2)
+    return dis  # How much of the 1000 meters it has fallen
+
+
+def loop():
+    # How tf do I verify this??
+    for i in range(100):
+        print(f"Position at {i} seconds: {calculate_position(i)}")
+        input()
+
+
+def time():
+    import math
+    t = math.sqrt(2 * 1000 / 9.81)
+    print(t)
+    print(calculate_position(t))
+
+
+def main():
+    print(calculate_position(14.27))
+
+
+if __name__ == "__main__":
+    time()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from phys!")
+
+
+if __name__ == "__main__":
+    main()
